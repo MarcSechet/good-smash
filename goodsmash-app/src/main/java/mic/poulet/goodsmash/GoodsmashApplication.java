279419beac5ecbf4cd13fac760f10c19d1b2fb2f
msechet <msechet@afoneinfrastructure.com>
2021-05-13 16:25:23 +0200

    #0 Initialisation multimodules

--- goodsmash-app/src/main/java/mic/poulet/goodsmash/GoodsmashApplication.java
+++ goodsmash-app/src/main/java/mic/poulet/goodsmash/GoodsmashApplication.java
@@ -2,6 +2,9 @@ package mic.poulet.goodsmash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class GoodsmashApplication {
@@ -9,5 +12,4 @@ public class GoodsmashApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GoodsmashApplication.class, args);
 	}
-
 }
