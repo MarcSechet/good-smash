89b87b6f0580ef021e6a6287bc11e9e1f74c521a
msechet <msechet@afoneinfrastructure.com>
2021-05-13 14:52:31 +0200

    #0 Initialisation projet

---
+++ src/main/java/mic/poulet/goodsmash/GoodsmashApplication.java
@@ -0,0 +1,13 @@
+package mic.poulet.goodsmash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsmashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsmashApplication.class, args);
+	}
+
+}
