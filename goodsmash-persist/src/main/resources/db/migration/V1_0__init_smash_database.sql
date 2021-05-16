909cdd0383fed22dd612d2ab826e4e2af1e631dd
msechet <msechet@afoneinfrastructure.com>
2021-05-16 12:50:46 +0200

    #4 Update sql

--- goodsmash-persist/src/main/resources/db/migration/V1_0__init_smash_database.sql
+++ goodsmash-persist/src/main/resources/db/migration/V1_0__init_smash_database.sql
@@ -193,21 +193,6 @@ create table move_inputs
 alter table move_inputs
     owner to postgres;
 
-create table playlist
-(
-    id            bigint not null
-        constraint playlist_pkey
-            primary key,
-    is_public     boolean,
-    playlist_id   varchar(255)
-        constraint uk_2cbvjl7skkh8n70jnh3k1ojb6
-            unique,
-    uploader_name varchar(255)
-);
-
-alter table playlist
-    owner to postgres;
-
 create table tier
 (
     id    bigint not null
@@ -289,20 +274,7 @@ create table timecode
 alter table timecode
     owner to postgres;
 
-create table playlist_timecodes
-(
-    playlist_id  bigint not null
-        constraint fk57siukrd69r44tlmmk40rcf6f
-            references playlist,
-    timecodes_id bigint not null
-        constraint uk_cdkmexq656cnfpenudcc6cbpg
-            unique
-        constraint fk4isu5fwcv30o0chjip2y30sn1
-            references timecode
-);
 
-alter table playlist_timecodes
-    owner to postgres;
 
 create table smash_user
 (
