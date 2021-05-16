91b4479262aa2bdb98b6cf4c8e8b79f5a5e9a1e8
msechet <msechet@afoneinfrastructure.com>
2021-05-16 12:57:41 +0200

    #5 Add pepites entities + dao + sql script

---
+++ goodsmash-persist/src/main/resources/db/migration/V1_1__init_pepites_database.sql
@@ -0,0 +1,30 @@
+create table playlist
+(
+    id            bigint not null
+        constraint playlist_pkey
+            primary key,
+    is_public     boolean,
+    playlist_id   varchar(255)
+        constraint uk_2cbvjl7skkh8n70jnh3k1ojb6
+            unique,
+    uploader_name varchar(255)
+);
+
+alter table playlist
+    owner to postgres;
+
+
+create table playlist_timecodes
+(
+    playlist_id  bigint not null
+        constraint fk57siukrd69r44tlmmk40rcf6f
+            references playlist,
+    timecodes_id bigint not null
+        constraint uk_cdkmexq656cnfpenudcc6cbpg
+            unique
+        constraint fk4isu5fwcv30o0chjip2y30sn1
+            references timecode
+);
+
+alter table playlist_timecodes
+    owner to postgres;
